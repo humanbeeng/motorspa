8ccb1fbef71487d7fc3493a8f0f858104b2455aa
Nithin <thenithinraj@gmail.com>
2023-08-20 18:18:56 +0530

    WIP Listing card

--- listings/ui/src/lib/components/ui/VehicleListingCard.svelte
+++ listings/ui/src/lib/components/ui/VehicleListingCard.svelte
@@ -22,39 +22,52 @@
 	});
 </script>
 
-<div
-	class="card p-3 border-1 grid grid-cols-5 justify-around h-full gap-5 shadow-sm w-full lg:w-1/2 lg:gap-3"
->
-	<div class="h-full col-span-2 border-1 lg:col-span-1 flex flex-col justify-center">
-		<img src="https://placehold.co/600x600" alt={data.model} class="h-auto lg:h-40 aspect-square" />
-	</div>
+<!-- <div -->
+<!-- 	class="card p-3 border-1 grid grid-cols-5 justify-around h-full gap-5 shadow-sm w-full lg:w-1/2 lg:gap-3" -->
+<!-- > -->
+<!-- 	<div class="h-full col-span-2 border-1 lg:col-span-1 flex flex-col justify-center"> -->
+<!-- 		<img src="https://placehold.co/600x600" alt={data.model} class="h-auto lg:h-40 aspect-square" /> -->
+<!-- 	</div> -->
 
-	<div
-		class="border-l-100 col-span-3 w-full h-full justify-between flex flex-col items-start lg:col-span-4 lg:px-6"
-	>
-		<div>
-			<p class="text-2xl font-semibold text-gray-800 lg:test-4xl">
-				{data.model},
-				<span class="text-lg lg:text-2xl">
-					{data.year_of_purchase.substring(0, 4)}
-				</span>
-			</p>
-			<p class="font-medium text-xl">{data.brand_name}</p>
-		</div>
+<!-- <div -->
+<!-- 	class="border-l-100 col-span-3 w-full h-full justify-between flex flex-col items-start lg:col-span-4 lg:px-6" -->
+<!-- > -->
+<!-- 	<div> -->
+<!-- 		<p class="text-2xl font-semibold text-gray-800 lg:test-4xl"> -->
+<!-- 			{data.model}, -->
+<!-- 			<span class="text-lg lg:text-2xl"> -->
+<!-- 				{data.year_of_purchase.substring(0, 4)} -->
+<!-- 			</span> -->
+<!-- 		</p> -->
+<!-- 		<p class="font-medium text-xl">{data.brand_name}</p> -->
+<!-- 	</div> -->
+<!---->
+<!-- <div class="grid grid-cols-1 justify-between w-full lg:grid-cols-2"> -->
+<!-- 	<div> -->
+<!-- 		<p class="text-sm">Kms: {data.kms}</p> -->
+<!-- 		<p class="text-sm">Location: {data.location}</p> -->
+<!-- 		<p class="text-sm">Color: {data.color}</p> -->
+<!-- 		<p class="text-sm">Price: {price}/-</p> -->
+<!-- 	</div> -->
+<!---->
+<!-- 			<div> -->
+<!-- 				<p class="text-sm">Owner: {data.owner_name}</p> -->
+<!-- 				<p class="text-sm">Phone: {data.owner_mobile_number}</p> -->
+<!-- 				<p class="text-sm">Fuel Type: {data.fuel_type}</p> -->
+<!-- 			</div> -->
+<!-- 		</div> -->
+<!-- 	</div> -->
+<!-- </div> -->
 
-		<div class="grid grid-cols-1 justify-between w-full lg:grid-cols-2">
-			<div>
-				<p class="text-sm">Kms: {data.kms}</p>
-				<p class="text-sm">Location: {data.location}</p>
-				<p class="text-sm">Color: {data.color}</p>
-				<p class="text-sm">Price: {price}/-</p>
-			</div>
-
-			<div>
-				<p class="text-sm">Owner: {data.owner_name}</p>
-				<p class="text-sm">Phone: {data.owner_mobile_number}</p>
-				<p class="text-sm">Fuel Type: {data.fuel_type}</p>
-			</div>
-		</div>
+<div class="shadow-sm border-gray-200 rounded-md border p-2 w-full flex flex-row gap-4 lg:w-1/2">
+	<img class="h-40 aspect-square flex-none" src="https://placehold.co/600x600" alt={data.model} />
+	<div class="flex-grow w-full">
+		<p class="text-sm">Kms: {data.kms}</p>
+		<p class="text-sm">Location: {data.location}</p>
+		<p class="text-sm">Color: {data.color}</p>
+		<p class="text-sm">Price: {price}/-</p>
+		<p class="text-sm">Owner: {data.owner_name}</p>
+		<p class="text-sm">Phone: {data.owner_mobile_number}</p>
+		<p class="text-sm">Fuel Type: {data.fuel_type}</p>
 	</div>
 </div>
