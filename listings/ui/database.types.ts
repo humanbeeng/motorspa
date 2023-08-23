7c5f33aef77aac14780f8ab838e684be0a0db4b3
Nithin <thenithinraj@gmail.com>
2023-08-23 10:34:31 +0530

    Update db types

--- listings/ui/database.types.ts
+++ listings/ui/database.types.ts
@@ -14,52 +14,52 @@ export interface Database {
           brand_name: string
           color: string | null
           created_at: string
-          fuel_type: Database["public"]["Enums"]["fuel_type"]
+          fuel_type: Database["public"]["Enums"]["fuel_type"] | null
           id: number
-          images: string[]
-          is_sold: boolean
+          images: string[] | null
+          is_sold: boolean | null
           kms: number
           location: string | null
           model: string
           note: string | null
-          owner_mobile_number: string | null
+          owner_mobile_number: string
           owner_name: string | null
-          price: number | null
-          year_of_purchase: string
+          price: number
+          year_of_purchase: number
         }
         Insert: {
           brand_name: string
           color?: string | null
           created_at?: string
-          fuel_type?: Database["public"]["Enums"]["fuel_type"]
+          fuel_type?: Database["public"]["Enums"]["fuel_type"] | null
           id?: number
-          images?: string[]
-          is_sold?: boolean
+          images?: string[] | null
+          is_sold?: boolean | null
           kms: number
           location?: string | null
           model: string
           note?: string | null
-          owner_mobile_number?: string | null
+          owner_mobile_number?: string
           owner_name?: string | null
-          price?: number | null
-          year_of_purchase: string
+          price: number
+          year_of_purchase: number
         }
         Update: {
           brand_name?: string
           color?: string | null
           created_at?: string
-          fuel_type?: Database["public"]["Enums"]["fuel_type"]
+          fuel_type?: Database["public"]["Enums"]["fuel_type"] | null
           id?: number
-          images?: string[]
-          is_sold?: boolean
+          images?: string[] | null
+          is_sold?: boolean | null
           kms?: number
           location?: string | null
           model?: string
           note?: string | null
-          owner_mobile_number?: string | null
+          owner_mobile_number?: string
           owner_name?: string | null
-          price?: number | null
-          year_of_purchase?: string
+          price?: number
+          year_of_purchase?: number
         }
         Relationships: []
       }
@@ -71,7 +71,7 @@ export interface Database {
       [_ in never]: never
     }
     Enums: {
-      fuel_type: "Diesel" | "Petrol" | "Hybrid"
+      fuel_type: "Petrol" | "Diesel" | "Hybrid"
     }
     CompositeTypes: {
       [_ in never]: never
